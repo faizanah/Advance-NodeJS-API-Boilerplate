1c91d868228a1ae8cd9f4f9e1a03c8a8bc0f39dc
Faizan AH <faizan@aiondigital.com>
2019-10-23 15:54:13 +0300

    typo fix

--- app/1.0/routes/customer.route.js
+++ app/1.0/routes/customer.route.js
@@ -2,5 +2,5 @@ import CustomerController from '../controllers/customers.controller';
 import CustomerValidator from '../validations/customer.validation';
 
 module.exports = (router) => {
-    router.get('/customers/:email/1.0',[CustomerValidator.emailVerify()], CustomerController.getByEmailID);
+    router.get('/customer/:email/1.0',[CustomerValidator.emailVerify()], CustomerController.getByEmailID);
 }
