b95576cdc76ce2a2b75682bf32b8413632661851
Faizan AH <faizan@aiondigital.com>
2019-10-24 11:28:50 +0300

    typo fixes

--- config/environments/production.js
+++ config/environments/production.js
@@ -4,7 +4,14 @@ if (result.error) {
     throw result.error;
 }
 module.exports = {
-    app: {
-        ENV_CDT_PORT:  process.env.ENV_CDT_PORT
+    APP: {
+        NAME: 'Customer Service',
+        ENV_CDT_PORT: process.env.ENV_CDT_PORT,
+        BASE_PATH: '/api/cb/bs/',
+        VERSION: '1.0'
+    },
+    DB: {
+        ENV_CDT_CONNECTION: '',
+        ENV_CDT_DATABASE_DEBUG: false
     }
 }
