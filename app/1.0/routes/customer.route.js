aabc9a8967d68f3b446404f8aa0991044b27d8e5
Faizan AH <faizan@aiondigital.com>
2019-10-29 13:59:25 +0300

    text changes

--- app/1.0/routes/customer.route.js
+++ app/1.0/routes/customer.route.js
@@ -1,6 +1,6 @@
-import customerController from '../controllers/customers.controller'
-import CustomerValidator from './../validations/customer.validation'
+import customerController from '../controllers/customers.controller';
+import CustomerValidator from './../validations/customer.validation';
 module.exports = (router) => {
-    router.get('/customer/:email/1.0', CustomerValidator.emailVerify(), customerController.getByEmailID);
-    router.post('/customer/1.0', CustomerValidator.createCustomerValidator(), customerController.createCustomer);
-}
+	router.get('/customer/:email/1.0', CustomerValidator.emailVerify(), customerController.getByEmailID);
+	router.post('/customer/1.0', CustomerValidator.createCustomerValidator(), customerController.createCustomer);
+};
