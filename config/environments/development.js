1b13f8a971c1e45c6d7f54e726eeb94aa1133383
Faizan AH <faizan@aiondigital.com>
2019-10-30 14:28:25 +0300

    update environment files

--- config/environments/development.js
+++ config/environments/development.js
@@ -1,17 +1,12 @@
-import * as dotenv from 'dotenv';
-const result = dotenv.config();
-if (result.error) {
-    throw result.error;
-}
 module.exports = {
-    APP: {
-        NAME: 'Customer Service',
-        ENV_CDT_PORT: process.env.ENV_CDT_PORT,
-        BASE_PATH: '/api/cb/bs/',
-        VERSION: '1.0'
-    },
-    DB: {
-        ENV_CDT_CONNECTION: '',
-        ENV_CDT_DATABASE_DEBUG: false
-    }
-}
+	APP: {
+		NAME: 'Customer Service',
+		ENV_CDT_PORT: process.env.ENV_CDT_PORT || process.env.PORT,
+		BASE_PATH: '/api/cb/bs/',
+		VERSION: '1.0'
+	},
+	DB: {
+		ENV_CDT_CONNECTION: '',
+		ENV_CDT_DATABASE_DEBUG: false
+	}
+};
