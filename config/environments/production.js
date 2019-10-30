3872be3a8085e1652e23ef80e9a7eeae05130d6a
Faizan AH <faizan@aiondigital.com>
2019-10-30 20:49:01 +0300

    typo fix

--- config/environments/production.js
+++ config/environments/production.js
@@ -8,11 +8,11 @@ module.exports = {
 	},
 	DB: {
 		USERNAME: process.env.ENV_CDT_DB_USERNAME,
-		PASSWORD: process.env.ENV_DB_PASSWORD,
+		PASSWORD: process.env.ENV_CDT_DB_PASSWORD,
 		HOST: process.env.ENV_CDT_DB_HOST,
-		DATABASE_NAME: process.env.ENV_DB_NAME,
-		PORT: process.env.ENV_DB_PORT,
-		DIALECT: process.env.ENV_DB_DIALECT,
+		DATABASE_NAME: process.env.ENV_CDT_DB_NAME,
+		PORT: process.env.ENV_CDT_DB_PORT,
+		DIALECT: process.env.ENV_CDT_DB_DIALECT,
 		ENV_CDT_DATABASE_DEBUG: false
 	}
 };
