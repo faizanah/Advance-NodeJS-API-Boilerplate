ab6fcce9e19e8b2e1f0fe6c1c56a0c6ae60c2b8d
Faizan AH <faizan@aiondigital.com>
2019-11-30 07:05:47 -0800

    update database cnnectin file

--- config/database/index.js
+++ config/database/index.js
@@ -1,24 +1,23 @@
 import knex from 'knex';
-import { AppSetting } from '../app.setting';
+import { AppSetting, Logger } from '..';
+
 const CONFIG = AppSetting.getConfig();
-console.log(CONFIG);
 const db = knex({
-	client: CONFIG.DB.DIALECT,
-	connection: AppSetting.getDBConnection(),
-	debug: CONFIG.DB.ENV_CDT_DATABASE_DEBUG,
-	pool: {
-		min: 0,
-		max: 7
-	}
+  client: CONFIG.DB.DIALECT,
+  connection: AppSetting.getDBConnection(),
+  debug: CONFIG.DB.ENV_CDT_DATABASE_DEBUG,
+  pool: {
+    min: 0,
+    max: 7
+  }
 });
-db.raw('select 1+1 as result').then((users) => 
-	console.log(`Successful connected with ${CONFIG.DB.DIALECT} database.`)
-).catch((err) => { 
-	console.log(err); 
-	process.exit(1);
-	throw err; 
-})
-.finally(() => {
-	//db.destroy();
-});
+db.raw('select 1+1 as result')
+  .then(() =>
+    Logger.debug(`Successful connected with ${CONFIG.DB.DIALECT} database.`)
+  )
+  .catch(err => {
+    Logger.error(err);
+    process.exit(1);
+    throw err;
+  });
 export default db;
