6bda5e66dadf9aa8f1bd03648b2ec1eb5b2f1709
Faizan AH <faizan@aiondigital.com>
2019-12-01 06:00:57 -0800

    update validation

--- app/1.0/validations/customer.validation.js
+++ app/1.0/validations/customer.validation.js
@@ -1,12 +1,27 @@
+import Joi from 'joi';
 import Validator from '../../../utilities/helpers/validator.helper';
 
-class CustomerValidation {
-    static emailVerify() {
-        // return Validator.params(CustomerSchema.verifyEmail);
-    }
-    static createCustomerValidator() {
-        // return Validator.body(CustomerSchema.createCustomerRequest);
-    }
-}
+const schema = {
+  email: Joi.string()
+    .email()
+    .lowercase()
+    .required()
+};
 
+const CustomerValidation = {
+  validate: type => {
+    switch (type) {
+      case 'email': {
+        const validation = Joi.object()
+          .keys({
+            customer_email: schema.email
+          })
+          .unknown(true);
+        return Validator.params(validation);
+      }
+      default:
+        return false;
+    }
+  }
+};
 export default CustomerValidation;
