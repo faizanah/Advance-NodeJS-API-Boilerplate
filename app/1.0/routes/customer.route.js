26d1ff3c30d642938576009c0528318e885a9656
Faizan AH <faizan@aiondigital.com>
2019-12-01 03:12:02 -0800

    dynamic routing fix

--- app/1.0/routes/customer.route.js
+++ app/1.0/routes/customer.route.js
@@ -1,4 +1,5 @@
 import customerController from '../controllers/customers.controller';
-module.exports = (router) => {
-	router.get('/customer/:email/1.0', customerController.getByEmailID);
+
+export default router => {
+  router.get('/customer/:email/1.0', customerController.getByEmailID);
 };
--- config/api.routing.js
+++ config/api.routing.js
@@ -1,31 +1,43 @@
-'use strict';
 import express from 'express';
-import {AppSetting} from '../config';
 import fs from 'fs';
+import AppSetting from './app.setting';
+import AppError from './app.error';
+import Logger from './logger';
+
 const router = express.Router();
 class ApiRouting {
-	static ConfigureRouters(app) {
-		const versions = this.getVersions();
-		for (let version of versions) {
-			const routes = this.getRoutesByVersion(version);
-			for (let route of routes) {
-				this.loadRoute(version, route);
-			}
-		}
-		app.use(AppSetting.getConfig().APP.BASE_PATH || '', router);
-	}
+  static async ConfigureRouters(app) {
+    const versions = this.getVersions();
+    versions.map(async version => {
+      const routes = this.getRoutesByVersion(version);
+      routes.map(async route => {
+        this.loadRoute(version, route);
+      });
+    });
+    app.use(AppSetting.getConfig().APP.BASE_PATH || '', router);
+  }
 
-	static getFile(path) {
-		return fs.readdirSync(path).filter(dir => !dir.match(/(^\.)|index/i));
-	}
-	static getVersions() {
-		return this.getFile(`${__dirname}/../app/`);
-	}
-	static getRoutesByVersion(version) {
-		return this.getFile(`${__dirname}/../app/${version}/routes`);
-	}
-	static loadRoute(version, route) {
-		require(`${__dirname}/../app/${version}/routes/${route}`)(router);
-	}
+  static getFile(path) {
+    return fs.readdirSync(path).filter(dir => !dir.match(/(^\.)|index/i));
+  }
+
+  static getVersions() {
+    return this.getFile(`${__dirname}/../app/`);
+  }
+
+  static getRoutesByVersion(version) {
+    return this.getFile(`${__dirname}/../app/${version}/routes`);
+  }
+
+  static loadRoute(version, route) {
+    import(`${__dirname}/../app/${version}/routes/${route}`)
+      .then(m => {
+        return m.default(router);
+      })
+      .catch(error => {
+        Logger.error(error);
+        throw new AppError('ERR_LOAD_ROUTE');
+      });
+  }
 }
 export default ApiRouting;
