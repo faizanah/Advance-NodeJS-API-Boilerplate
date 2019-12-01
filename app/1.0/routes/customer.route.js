de895fd93cd13840cdb9b5e198a8115a808540e3
Faizan AH <faizan@aiondigital.com>
2019-12-01 06:01:31 -0800

    update router

--- app/1.0/routes/customer.route.js
+++ app/1.0/routes/customer.route.js
@@ -1,5 +1,10 @@
-import customerController from '../controllers/customers.controller';
+import CustomerController from '../controllers/customers.controller';
+import CustomerValidator from '../validations/customer.validation';
 
 export default router => {
-  router.get('/customer/:email/1.0', customerController.getByEmailID);
+  router.post(
+    '/customer/email/:customer_email/1.0',
+    CustomerValidator.validate('email'),
+    CustomerController.validateEmail
+  );
 };
