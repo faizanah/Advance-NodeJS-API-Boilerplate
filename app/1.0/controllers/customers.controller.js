1daef2963eae0b189d8d1dfe784868618eac3bb5
Faizan AH <faizan@aiondigital.com>
2019-12-01 06:31:08 -0800

    typo fixes

--- app/1.0/controllers/customers.controller.js
+++ app/1.0/controllers/customers.controller.js
@@ -1,16 +1,14 @@
 import CustomerService from '../services/customer.service';
-import Api from '../../../lib/api'
+import Api from '../../../lib/api';
+
 class CustomersController {
-
-    constructor() { }
-
-    getByEmailID(request, response) {
-        try {
-            let result = CustomerService.getByEmail();
-            Api.ok(request, response, result);
-        } catch (err) {
-            Api.serverError(request, response, err);
-        }
+  static async getByEmailID(request, response, next) {
+    try {
+      const result = CustomerService.getByEmail();
+      Api.ok(request, response, result);
+    } catch (err) {
+      next(err);
     }
+  }
 }
-export default new CustomersController();
+export default CustomersController;
--- app/1.0/services/customer.service.js
+++ app/1.0/services/customer.service.js
@@ -1,9 +1,13 @@
 class CustomerService {
-    constructor() { }
-    getByEmail() {
-        let response = { customerID: '1245', status: 'Active', createdOn: new Date(), others: {} };
-        return response;
-    }
+  static async getByEmail() {
+    const response = {
+      customerID: '1245',
+      status: 'Active',
+      createdOn: new Date(),
+      others: {}
+    };
+    return response;
+  }
 }
 
-export default new CustomerService();
+export default CustomerService;
